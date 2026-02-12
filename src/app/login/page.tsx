45bbf15fb4d223226301df5c898c2ff676c066e4
tangentkoh <k.hori.672@stn.nitech.ac.jp>
2026-02-12 10:58:17 +0900

    爆破

--- src/app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import AuthForm from "@/components/AuthForm";
-
-export default function LoginPage() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-50">
-      <AuthForm />
-    </div>
-  );
-}
